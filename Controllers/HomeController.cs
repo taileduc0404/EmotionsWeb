d391343a40004f8aecfa2e042f9b2ebad8fbfea4
taileduc0404 <tai.leduc040402@gmail.com>
2024-01-17 09:41:56 +0700

    Display Review, Pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,47 @@
-﻿using CamXucWeb.Models;
+﻿using CamXucWeb.Data;
+using CamXucWeb.Models;
+using CamXucWeb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.WebSockets;
+using X.PagedList;
 
 namespace CamXucWeb.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger,
+            ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult Index(int page=1, int pageSize=10)
+        {
+            var review = _context.reviews?.ToPagedList(page, pageSize);
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+            var vm = new HomeVM()
+            {
+                Reviews = review
+            };
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
---
+++ ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using CamXucWeb.Models;
+using X.PagedList;
+
+namespace CamXucWeb.ViewModels
+{
+    public class HomeVM
+    {
+        public IPagedList<Review>? Reviews { get; set; }
+    }
+}
